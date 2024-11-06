837ba0560a157c06846289a50db6da9d22ec21ee
Micael-p <micaelpessoa5@gmail.com>
2024-11-06 20:05:54 -0300

    Pag_Formulario.js

---
+++ Trabalho_HTML/Pag_Formulario.js
@@ -0,0 +1,20 @@
+function validar() {
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const nomeErro = document.getElementById('nomeErro');
+    
+    nomeErro.textContent = '';
+
+    if (nome.trim() === '') {
+        nomeErro.textContent = 'O nome é obrigatório.';
+        return false;
+    }
+
+    if (email.trim() === '') {
+        nomeErro.textContent = 'O email é obrigatório.';
+        return false;
+    }
+
+    window.location.href = 'Menu_Princ.html';
+    return false;
+}
